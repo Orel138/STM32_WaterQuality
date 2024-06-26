f72d5f8298fc89373490f2c2fb2e8091b54b74f5
Orel138 <orel.reynaud@gmail.com>
2024-06-26 23:38:02 +0200

    Update application with a 1st draft for the gauge

--- Projects/32L4R9IDISCOVERY/Applications/WaterQualityStation/TouchGFX/gui/src/main_screen/MainView.cpp
+++ Projects/32L4R9IDISCOVERY/Applications/WaterQualityStation/TouchGFX/gui/src/main_screen/MainView.cpp
@@ -22,7 +22,7 @@ void MainView::setupScreen()
     HAL::getInstance()->setFrameRateCompensation(true);
 
     // Set hands to initial positions
-    analogClock.setTime24Hour(hours, minutes, seconds);
+//    analogClock.setTime24Hour(hours, minutes, seconds);
 }
 
 void MainView::tearDownScreen()
@@ -32,34 +32,37 @@ void MainView::tearDownScreen()
 
 void MainView::handleTickEvent()
 {
-    ++tickCounter;
-
-    if (tickCounter % 108 == 0)
-    {
-        if (++seconds >= 60)
-        {
-            seconds = 0;
-            if (++minutes >= 60)
-            {
-                minutes = 0;
-                if (++hours >= 24)
-                {
-                    hours = 0;
-                }
-            }
-        }
-
-        // Update humidity
-        humidityEntropi = ((seconds * seconds + seconds * 5 + hours) % 180) / 360.0f * 2 * PI - PI / 2;
-        humidity.setupAnimation(AnimationTextureMapper::Z_ROTATION, humidityEntropi, 250, 0, EasingEquations::cubicEaseInOut);
-        humidity.startAnimation();
-
-        // Update pressure
-        pressureEntropi = (((seconds + minutes + hours) * (seconds + minutes + hours)) % 180) / 360.0f * 2 * PI - PI / 2;
-        pressure.setupAnimation(AnimationTextureMapper::Z_ROTATION, pressureEntropi, 250, 0, EasingEquations::cubicEaseInOut);
-        pressure.startAnimation();
-
-        // Update the clock
-        analogClock.setTime24Hour(hours, minutes, seconds);
-    }
+//    ++tickCounter;
+//
+//    if (tickCounter % 108 == 0)
+//    {
+//        if (++seconds >= 60)
+//        {
+//            seconds = 0;
+//            if (++minutes >= 60)
+//            {
+//                minutes = 0;
+//                if (++hours >= 24)
+//                {
+//                    hours = 0;
+//                }
+//            }
+//        }
+//
+//        // Update humidity
+//        humidityEntropi = ((seconds * seconds + seconds * 5 + hours) % 180) / 360.0f * 2 * PI - PI / 2;
+//        humidity.setupAnimation(AnimationTextureMapper::Z_ROTATION, humidityEntropi, 250, 0, EasingEquations::cubicEaseInOut);
+//        humidity.startAnimation();
+//
+//        // Update pressure
+//        pressureEntropi = (((seconds + minutes + hours) * (seconds + minutes + hours)) % 180) / 360.0f * 2 * PI - PI / 2;
+//        pressure.setupAnimation(AnimationTextureMapper::Z_ROTATION, pressureEntropi, 250, 0, EasingEquations::cubicEaseInOut);
+//        pressure.startAnimation();
+//
+//        // Update the clock
+////        analogClock.setTime24Hour(hours, minutes, seconds);
+//
+//        gauge1.updateValue(gauge1.getValue() + 1, 0);
+//    }
+	gauge1.updateValue(gauge1.getValue() + 1, 0);
 }
